479cf585288f1142334a3bcd5ac383d14d82752a
Sarthak Kuchhal <developersbeat@gmail.com>
2021-10-10 01:50:02 +0530

    Variables, Functions

---
+++ Day1/VariableFunctionsLoops.js
@@ -0,0 +1,40 @@
+//Binding
+let a = 5 * 5;
+
+
+let ten = 10;
+console.log(ten * ten);
+//100
+
+//  " = " is the assigning operator
+
+
+let time = "day";
+console.log(time);
+//day
+
+time = "night";
+console.log(time);
+//night
+
+
+//The word "const" stands for constant. It defines a constant binding, which points at the same value for as long as it lives.
+
+const $greeting = "Hello ";
+console.log($greeting);
+//Hello
+
+//Note : keywords such as let, const, var and so on are reserved words. CANNOT BE USED AS A VARIABLE
+
+
+
+
+
+
+
+
+//Functions
+
+prompt("Enter Passcode");
+console.log("function");
+console.log(Math.max(2, 4));
