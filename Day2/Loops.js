b2138cc29e19753c68f1c764e34602b91a570251
Sarthak Kuchhal <developersbeat@gmail.com>
2021-10-10 16:58:22 +0530

    forloops and shorthands

--- Day2/Loops.js
+++ Day2/Loops.js
@@ -1,12 +1,12 @@
 //while and do loops
 
 let number = 0;
-while(number <= 10){
-    console.log(number);
-    number = number + 1;
+while (number <= 10) {
+  console.log(number);
+  number = number + 1;
 }
 
-// output: 
+// output:
 //  1
 //  2
 //  3
@@ -18,13 +18,34 @@ while(number <= 10){
 //  9
 //  10
 
-
-
 //Do Loop
 let yourName;
-do{
-    yourName = prompt("Who are you?");
+do {
+  yourName = prompt('Who are you?');
 } while (!yourName);
-console.log(yourName)
+console.log(yourName);
 
 // would re prompt the popup unless is the value is not empty
+
+// For Loops
+
+for (let number = 0; number < 10; number = number + 2) {
+  console.log(number);
+}
+
+// output :
+// 0
+// 2
+// 4
+// 6
+// 8
+
+//break the loop without completing the whole iteration -> use break
+for (let a = 1; a < 12; a = a + 1) {
+    if(a % 8 == 0){
+        console.log(a);
+        break;
+    }
+}
+//output : 8;
+
---
+++ Day2/Shorthands.js
@@ -0,0 +1,14 @@
+// number = number + 1; 
+// number += 1;
+
+
+// number = number - 1; 
+// number -= 1;
+
+
+
+// number = number * 2; 
+// number *= 2;
+
+
+//pairs means the same
