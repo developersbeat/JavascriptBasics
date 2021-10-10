d97e4ae6fd6121fbd11995f852ad3f6a0c03bab6
Sarthak Kuchhal <developersbeat@gmail.com>
2021-10-10 17:00:29 +0530

    switch statements

---
+++ Day2/Switch.js
@@ -0,0 +1,16 @@
+switch (prompt("What is the weather like?")) {
+    case "rainy":
+      console.log("Remember to bring an umbrella.");
+      break;
+    case "sunny":
+      console.log("Dress lightly.");
+    case "cloudy":
+      console.log("Go outside.");
+      break;
+    default:
+      console.log("Unknown weather type!");
+      break;
+  }
+
+
+  //can be usefull instead of if else IN SOME CASES.
