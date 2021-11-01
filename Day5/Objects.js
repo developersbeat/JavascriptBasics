ae8360bcb8ac6a4786fdac9e01bb1e5bfc7f7287
Sarthak Kuchhal <developersbeat@gmail.com>
2021-11-01 17:02:03 +0530

    objects added

---
+++ Day5/Objects.js
@@ -0,0 +1,59 @@
+let day1 = {
+    squirrel: false,
+    events: ["work", "touch tree", "pizza", "running"],
+    numbers: 34
+}
+
+console.log(day1.squirrel);
+// → false
+
+console.log(day1.numbers);
+//34 
+
+console.log(day1.wolf);
+// → undefined
+day1.wolf = false;
+console.log(day1.wolf);
+// → false
+
+console.log("object", day1);
+
+//=====================================================================
+let descriptions = {
+    work: "Went to work",
+    "touched tree": "Touched a tree"
+};
+  
+
+console.log(Object.keys(day1)) 
+// Get all the key of the object day1 using Object.keys
+// squirrel , events , numbers , wolf
+// (Wolf was added later to the day1 object)
+
+
+
+//Object.assign
+
+let objectA = { a: 1, b: 2 };
+Object.assign(objectA, { b: 3, c: 4 });
+console.log(objectA);
+// → {a: 1, b: 3, c: 4}
+
+//modified value of b & added new key value pair c:4
+
+let journal = [
+    {events: ["work", "touched tree", "pizza",
+              "running", "television"],
+     squirrel: false},
+    {events: ["work", "ice cream", "cauliflower",
+              "lasagna", "touched tree", "brushed teeth"],
+     squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts",
+              "beer"],
+     squirrel: true},
+    /* and so on... */
+];
+console.log("\n\n\n\n\n NEW OBJECT JOURNAL")
+console.log("journal array:",journal)
+console.log("journal array 2nd element",journal[1])
+console.log("journal array 2nd element's events object",journal[1].events)
